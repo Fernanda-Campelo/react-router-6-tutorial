ba228e44b234535faafc6a76621f4ed215c7a464
Fernanda Campelo <fernanda.cstdias@gmail.com>
2023-06-20 13:23:21 -0300

    implementing useNavigate

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -6,8 +7,12 @@ import Products from "./pages/Products";
 import ErrorPage from "./pages/Error";
 import SharedLayout from "./pages/SharedLayout";
 import SingleProduct from "./pages/SingleProduct";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -16,6 +21,11 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="products" element={<Products />} />
           <Route path="products/:productId" element={<SingleProduct />} />
+          <Route path="login" element={<Login setUser={setUser}></Login>} />
+          <Route
+            path="dashboard"
+            element={<Dashboard user={user}></Dashboard>}
+          />
           <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
