b688350020c8b3bb0ba8dc6c0de25c3181927296
Fernanda Campelo <fernanda.cstdias@gmail.com>
2023-06-20 12:00:50 -0300

    implementing the  shared layout

--- src/App.js
+++ src/App.js
@@ -8,14 +8,13 @@ import ErrorPage from "./pages/Error";
 function App() {
   return (
     <BrowserRouter>
-      <nav>our navbar</nav>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="products" element={<Products />} />
-        <Route path="*" element={<ErrorPage />} />
+        <Route path="/" element={<Home />}>
+          <Route path="about" element={<About />} />
+          <Route path="products" element={<Products />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
       </Routes>
-      <footer>our footer</footer>
     </BrowserRouter>
   );
 }
